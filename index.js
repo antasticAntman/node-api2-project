a3ad95e367b8144df7c6c671098f053823f74774
Anthony Climer <anthonyclimer0925@gmail.com>
2023-04-21 13:42:10 -0400

    I have completed the server setup

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const express = require('express');
+
+const server = require('./api/server') 
+
+const port = 5000
+
+server.listen(port, () => {
+    console.log('listening on ', port) 
+})
